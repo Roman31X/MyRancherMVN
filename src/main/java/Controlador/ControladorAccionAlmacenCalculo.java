cd2ef4ac94ef3d190b4e57d7a15aed1a0f9df8cd
Roman31X <die.haa65@gmail.com>
2023-07-15 03:54:16 -0500

    CalculoALIMENTOGANADO

--- src/main/java/Controlador/ControladorAccionAlmacenCalculo.java
+++ src/main/java/Controlador/ControladorAccionAlmacenCalculo.java
@@ -9,7 +9,8 @@ import java.util.List;
 public class ControladorAccionAlmacenCalculo implements ActionListener{
     private VistaAlmacenAlimentoCalculo calculo;
     private List<GanadoDTO> listGanado;
-
+    
+    
     public ControladorAccionAlmacenCalculo(VistaAlmacenAlimentoCalculo calculo2, List<GanadoDTO> listGanado2) {
         this.calculo = calculo2;
         this.listGanado = listGanado2;
@@ -21,11 +22,50 @@ public class ControladorAccionAlmacenCalculo implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        double edad;
+        int contadorAdulto = 0;
+        int contadorTernero = 0;
+        
         String accion = e.getActionCommand();
         System.out.println("accion = " + accion);
         switch(accion){
             case"comboBoxChanged":
-                
+                String tipo = calculo.Tipo.getSelectedItem().toString();
+                System.out.println("tipo = " + tipo);
+                switch(tipo){
+                    case "-Seleccion-":
+                        calculo.CantidadAdulto.setText("");
+                        calculo.CantidadTernero.setText("");
+                        break;
+                    case "Lechero":
+                        for (GanadoDTO lista : listGanado) {
+                            if(lista.getTipoGanado().equals("Lechero")){
+                                edad = Double.parseDouble(lista.getEdad());
+                                if(edad >= 1){
+                                    contadorAdulto++;
+                                }else{
+                                    contadorTernero++;
+                                }
+                            }
+                        }
+                        calculo.CantidadAdulto.setText(String.valueOf(contadorAdulto));
+                        calculo.CantidadTernero.setText(String.valueOf(contadorTernero));
+                        break;
+                    case "Engorde":
+                        for (GanadoDTO lista : listGanado) {
+                            if(lista.getTipoGanado().equals("Engorde")){
+                                edad = Double.parseDouble(lista.getEdad());
+                                if(edad > 1){
+                                    contadorAdulto++;
+                                }else{
+                                    contadorTernero++;
+                                }
+                            }
+                        }
+                        calculo.CantidadAdulto.setText(String.valueOf(contadorAdulto));
+                        calculo.CantidadTernero.setText(String.valueOf(contadorTernero));
+                        break;
+                }
                 break;
             case"CALCULAR":
                 break;
